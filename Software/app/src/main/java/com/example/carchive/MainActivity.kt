7d0dd8ac8383dd22a321487c9f53ec5eb3c3077f
ppazin <ppazin22@student.foi.hr>
2024-11-15 19:07:23 +0100

    Popravljanje boja i dodavanje ikonica

--- Software/app/src/main/java/com/example/carchive/MainActivity.kt
+++ Software/app/src/main/java/com/example/carchive/MainActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.carchive.fragments.LoginFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         setContentView(R.layout.activity_auth)
         enableEdgeToEdge()
 
